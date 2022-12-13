3db40fa31668a91b25e71f960108f4274dfc1d2f
tuan.le <tuan.le@gosmartlog.com>
2022-12-13 16:48:37 +0700

    setup mui v5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,31 @@
-import '../styles/globals.css'
+import axiosClient from '@/api/axios-client'
 import { EmptyLayout } from '@/components/layouts'
 import { AppPropsWithLayout } from '@/models/common'
+import { CacheProvider } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import { SWRConfig } from 'swr'
-import axiosClient from '@/api/axios-client'
+import { createEmotionCache, theme } from 'utils'
+import '../styles/globals.css'
 
-const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+const clientSideEmotionCache = createEmotionCache();
 
-  const Layout = Component.Layout ?? EmptyLayout
+const App = ({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppPropsWithLayout) => {
+    
+    const Layout = Component.Layout ?? EmptyLayout
 
-  return (
-    <SWRConfig value={{
-      fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false
-    }}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SWRConfig>
-  )
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <SWRConfig value={{ fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false }}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </SWRConfig>
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
 
 export default App
